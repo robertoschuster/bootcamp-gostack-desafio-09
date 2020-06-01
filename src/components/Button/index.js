ae8f445c84592df689a0cbb0100e8efb75ae69ab
robertoschuster <robertoaschuster@gmail.com>
2020-06-01 09:58:52 -0300

    Inclusão de efeito "hover" no componente Button.

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,22 +1,31 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Button = styled.button`
   background: ${(props) => (props.primary ? '#7d40e7' : '#999')};
-  /* background: lightseagreen; */
   border: 0;
   padding: 10px 30px;
   border-radius: 4px;
   color: #fff;
   font-weight: bold;
+  margin-left: 10px;
 
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
 
-  margin-left: 10px;
-
   svg {
     margin-right: 6px;
+  }
+
+  transition: background 0.2s;
+  &:hover {
+    background: ${(props) =>
+      props.primary ? darken(0.06, '#7d40e7') : darken(0.06, '#999')};
+  }
+  &:active {
+    background: ${(props) =>
+      props.primary ? darken(0.2, '#7d40e7') : darken(0.2, '#999')};
   }
 `;
