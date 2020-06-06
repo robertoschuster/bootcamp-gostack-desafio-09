d8bae84183ed53cc519fc6a5f1f37d72565da5d6
robertoschuster <robertoaschuster@gmail.com>
2020-06-06 10:01:33 -0300

    fix: remove useless link

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
@@ -36,7 +35,6 @@ function SignIn() {
         <button type="submit">
           {loading ? 'Carregando...' : 'Entrar no sistema'}
         </button>
-        <Link to="/register">Criar conta gratuita</Link>
       </Form>
     </>
   );
